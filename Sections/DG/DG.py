0fd5c81df2ee9dd4a3701cd47436aefe54c37cd9
iamlrk <lepakshi.lepakshi@outlook.com>
2021-06-04 00:47:52 +0530

    Added Assets for Panda3D
    Co-Authored-By: iamThoshitha <84404265+iamThoshitha@users.noreply.github.com>

--- Sections/DG/DG.py
+++ Sections/DG/DG.py
@@ -1,2 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import VPCO as vp
+
+
+
+
---
+++ Sections/main.py
@@ -0,0 +1,4 @@
+from Sections import VPCO
+
+a = VPCO.GlobalCalculation.force_at_this_point(20000, 5.972e24, 1.989e30)
+print(a)
