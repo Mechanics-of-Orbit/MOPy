060443e6eb23bfc0af1f55063572bbc5a1e79ccc
iamlrk <lepakshi.lepakshi@outlook.com>
2021-06-04 18:28:26 +0530

    error correction

--- 3D_Programs/SOI3Dviz.py
+++ 3D_Programs/SOI3Dviz.py
@@ -13,7 +13,7 @@ class SOI(ShowBase):
     def __init__(self, planet):
         super().__init__()
 
-        self.camera.setPos(0,-1000,0)
+        self.camera.setPos(0,-500,0)
         
         self.stars = self.loader.loadModel("Assets/solar_sky_sphere")
         self.stars.setScale(40)
@@ -22,12 +22,13 @@ class SOI(ShowBase):
         self.stars.setTexture(self.stars_tex,1)
 
         self.planet_name = planet
-        self.dire = "Assets\hi_res_tex\a_"
+        self.dire = "Assets\hi_res_tex" + r'\a_'
         self.ComDir = self.dire + self.planet_name + ".jpg"
         self.MinorBody = self.loader.loadModel("Assets/planet_sphere")
         self.MinorBody.setScale(4)
         self.MinorBody_tex = loader.loadTexture(self.ComDir)
         self.MinorBody.setTexture(self.MinorBody_tex, 1)
+        self.MinorBody.reparentTo(render)
 
 
 
