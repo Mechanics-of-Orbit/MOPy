84477e30658f7e29e2d2af40968893e2f3433b2f
Manjunath-Aero <71602680+Manjunath-Aero@users.noreply.github.com>
2021-06-24 19:55:39 +0530

    Revert "c"
    This reverts commit 7b8a3018e3123d6048b69734b75dcf0e544ba804.

--- UI_Functions/Home_Page.py
+++ UI_Functions/Home_Page.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'Home_PageypkcUh.ui'
+## Form generated from reading UI file 'Home_PageVrYLbz.ui'
 ##
 ## Created by: Qt User Interface Compiler version 5.14.1
 ##
