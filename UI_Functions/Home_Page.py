a5539437a7726c5fcb261be111e8222f1f3f5b43
Manjunath-Aero <71602680+Manjunath-Aero@users.noreply.github.com>
2021-07-14 13:28:05 +0530

    little changes in GUI

--- UI_Functions/Home_Page.py
+++ UI_Functions/Home_Page.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'Home_PageqswMoC.ui'
+## Form generated from reading UI file 'Home_PageSxBneF.ui'
 ##
 ## Created by: Qt User Interface Compiler version 5.15.2
 ##
