68f3e440b11041845e2b878e2e24641c5594dfc7
iamlrk <lepakshi.lepakshi@outlook.com>
2022-03-23 22:17:04 +0530

    Added Planetary Ephemeris data and algo,  Eccentric Anomaly algo

---
+++ Functions/Sections/KepECCANewtMeth.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+# E - esin(E) = M
+def keplerECCA(ecc, MeAnom)
+    error = 1e-90
+
+    if M < np.pi
+        ECCA = MeAnom + ecc/2
+    else
+        ECCA = MeAnom - ecc/2
+    
+    ratio = 1
+    while abs(ratio) > error
+        ratio = (ECCA - ecc*np.sin(E) - MeAnom)/(1 - ecc*np.cos(ECCA))
+        ECCA = ECCA - ratio
+    return ECCA
--- Functions/trial.py
+++ Functions/trial.py
@@ -1,23 +1,33 @@
-from panda3d.core import loadPrcFile
-loadPrcFile("config/conf.prc")
+# from panda3d.core import loadPrcFile
+# loadPrcFile("config/conf.prc")
 
-from direct.showbase.ShowBase import ShowBase
-from ModelLoader import loadMyModel
+# from direct.showbase.ShowBase import ShowBase
+# from ModelLoader import loadMyModel
 
-class MyApp(ShowBase):
+# class MyApp(ShowBase):
 
-    def __init__(self):
-        ShowBase.__init__(self)
+#     def __init__(self):
+#         ShowBase.__init__(self)
 
-        load_my_model = loadMyModel()
-        model = "earth"
-        self.main_model = load_my_model.body(base, model)
-        self.main_model.reparentTo(render)
+#         load_my_model = loadMyModel()
+#         model = "earth"
+#         self.main_model = load_my_model.body(base, model)
+#         self.main_model.reparentTo(render)
 
-        self.cam.setPos(0,-100,0)
-        # self.sol_sky = load_my_model.solar_sky(base)
-        # self.sol_sky.reparentTo(render)
+#         self.cam.setPos(0,-100,0)
+#         # self.sol_sky = load_my_model.solar_sky(base)
+#         # self.sol_sky.reparentTo(render)
 
 
-app = MyApp()
-app.run()
+# app = MyApp()
+# app.run()
+
+# [a, b] = [[1, 2, 3, 4], [5, 6, 7, 8]]
+import numpy as np
+a = [1, 2, 3, 4]
+b = [5, 6, 7, 8]
+c = a + 2*np.array(b)
+print(a)
+print(b*4)
+print(np.sin(c))
+print(c[1])
